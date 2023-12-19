106ae1a45fd3a4cd84b4345c5c0c38eebbab419c
rebs-code <jack.rebussi@gmail.com>
2023-12-19 12:06:42 +0100

    add: League Gothic font for titles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script>
 </script>
 
-<template></template>
+<template>
+  <h1>Hello world i am a font</h1>
+  <p>hello world i am a font</p>
+</template>
 
 <style scoped lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 // import style.scss
 import "./assets/scss/style.scss";
+//import google font
+import "@fontsource/league-gothic";
 //import vue starting file
 import App from "./App.vue";
 
