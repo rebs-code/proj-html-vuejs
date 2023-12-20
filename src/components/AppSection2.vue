2df3337d65a41069a9f06cd3417dd45b424e57c5
rebs-code <jack.rebussi@gmail.com>
2023-12-20 14:14:12 +0100

    add: bottom card style in AppSection2

--- src/components/AppSection2.vue
+++ src/components/AppSection2.vue
@@ -9,13 +9,13 @@ export default {
 
 <template>
     <div class="app-section-2">
+        <!-- container -->
         <div class="container py-5">
             <div class="title d-flex flex-column align-items-center mb-5">
                 <h2 class="py-5 fst-italic">What critics are saying about us</h2>
                 <hr class="hr">
             </div>
-
-
+            <!-- top section -->
             <div class="col-12 section2-top d-flex justify-content-end">
                 <div class="col-6 left d-flex align-items-center justify-content-center">
                     <AppCardBlackSecondary cardCritic="Meggy Stewart" cardTitle="New York Times"
@@ -23,19 +23,25 @@ export default {
                         cardTextSecondary="Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi."
                         buttonText="Read full article" class="cardBlack" />
                 </div>
+            </div>
+            <!-- bottom section -->
+            <div class="col-12 section2-bottom">
+                <div class="bottom-card">
+                    <AppCardBlackSecondary cardCritic="Meggy Stewart" cardTitle="New York Times"
+                        cardText="Non arcu mauria tortor iutrices mollis tellus euismod fermentum. Habitant amet ticindunt id sapien accusamen sed at."
+                        cardTextSecondary="Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi."
+                        buttonText="Read full article" class="cardBlack" />
 
-
+                </div>
+                <div class="bottom-card">
+                    <AppCardBlackSecondary cardCritic="Meggy Stewart" cardTitle="New York Times"
+                        cardText="Non arcu mauria tortor iutrices mollis tellus euismod fermentum. Habitant amet ticindunt id sapien accusamen sed at."
+                        cardTextSecondary="Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi."
+                        buttonText="Read full article" class="cardBlack" />
+                </div>
             </div>
 
 
-            <div class="col-4 try">
-
-
-            </div>
-            <div class="col-4 try">
-
-
-            </div>
         </div>
 
 
@@ -65,6 +71,23 @@ export default {
 
     .left {
         background-color: black;
+    }
+}
+
+.section2-bottom {
+    height: 715px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 60px;
+
+    background-color: rgb(78, 49, 49);
+
+    .bottom-card {
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 47%;
     }
 }
 </style>
