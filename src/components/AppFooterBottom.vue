9155a55a02bdb34619a153a3f7994f71a566de4f
rebs-code <jack.rebussi@gmail.com>
2023-12-21 13:09:52 +0100

    feat: footer bottom

--- src/components/AppFooterBottom.vue
+++ src/components/AppFooterBottom.vue
@@ -4,6 +4,26 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="footer-bottom d-flex align-items-center justify-content-center">
+        <p class="text-size mb-0">Copyright 2012 - 2019 Avada | All rights Reserved | Powered by <span
+                class="fw-bold text-white">WordPress | ThemeFusion</span>
+        </p>
+    </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use '../assets/scss/partials/variables' as *;
+
+.footer-bottom {
+    padding: 30px;
+    background-color: black;
+
+    p {
+        color: $silver;
+        font-size: 1rem
+    }
+
+    ;
+}
+</style>
